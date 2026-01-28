ad6358f607e4694fbbe96ccafbf5dcb26fe0e0db
SapphicBear <sleepyscreenart@gmail.com>
2026-01-28 17:19:07 +0100

    Feat(gameHandler): Start creating gamehandler
    Start development on gameHandler function to take care of game variables
    and turns when a game is started. Very barebones

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -4,3 +4,17 @@ import { Carrier, Cruiser, Battleship, Scout } from "./ship.js";
 import "../styles.css";
 
 console.log("Success! Javascript connected!");
+
+const gameHandler = () => {
+    // get name from user, make characters
+    // const player = new Player(username);
+    // const cpu = new CPU();
+    let gameReady = false;
+    // listen for placing ships
+    // once all ships are placed, run player.finalPlace();
+    // then run cpu.initializeBoard();
+    // set gameReady = true;
+    const turn = () => {
+        // keep track of who's turn it is. 
+    };
+};
